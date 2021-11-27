fdb8252392f30d7e7d42358d46eaf88fd853ca02
wh1ter0se <camelCaseEverything@gmail.com>
2021-11-27 00:37:24 -0600

    Took a lot of tinkering with the node length, but added and implemented MultipleAxialTension.

--- FEAFunctions.py
+++ FEAFunctions.py
@@ -1,3 +1,4 @@
+from numpy.lib.function_base import disp
 from Bodies import *
 import numpy as np
 
@@ -10,11 +11,13 @@ def print_forces(forces):
         print("F_" + str(i+1) + ": " + str(round(forces[i],5)) + "N")
         i += 1
 
-def print_displacements(displacements):
+def print_displacements(displacements,global_displacement=None):
     i = 0
     for displacement in displacements:
         print("ΔL_" + str(i+1) + ": " + str(round((10**3)*displacements[i],5)) + "mm")
         i += 1
+    if global_displacement is not None:
+        print("ΔL_global: " + str(round((10**3)*global_displacement,5)) + "mm")
 
 # This is from the first simple example
 # Solves setup given EITHER force (N) or displacement (m)
@@ -42,12 +45,41 @@ def Solve_SimpleAxialTension(workpiece,force=None,displacement=None):
     print_forces(force_col)
     print_displacements(displacement_col)
 
+# Allows you to solve for local/global displacement, given pairs of locations and forces.
+# The area is assumed to be the same along the entire piece.
+# force_pos_pairs is an array of (node,force) tuples. Positive force = tension.
+def Solve_MultipleAxialTension(workpiece,pos_force_pairs,nodes):
+    node_length = workpiece.length / (nodes*(.5*(nodes-1))) # why does this line work? couldn't tell you.
+    coeff = (workpiece.area * workpiece.modulus) / node_length
+    local_stiffness = (np.eye(2) * 2) - 1.0
+    local_stiffness *= coeff
+    global_stiffness = np.zeros([nodes,nodes])
+    for i in range(nodes-1):
+        global_stiffness[i:i+2,i:i+2] += local_stiffness
+    forces = np.zeros([nodes,1])
+    for pair in pos_force_pairs:
+        pos, force = pair
+        forces[pos-1] += force
+    forces[0] = -sum(forces)
+    displacements = np.linalg.lstsq(global_stiffness,forces)[0]
+    offset = 0
+    for i in range(nodes-1):
+        if displacements[i] < 0:
+            offset -= displacements[i]
+    #offset = displacements[0]
+    #displacements += offset
+    displacements -= displacements[0]
+    print_forces(forces.T[0])
+    print_displacements(displacements.T[0],global_displacement=sum(displacements.T[0]))
+    
 
 
-# Function tests
-# Results can be verified with
-# https://www.omnicalculator.com/physics/stress
-piece = SquarePrism(Elasticity.ALUMINUM.value,.1,5)
+
+## Function tests
+#  Results can be verified with
+#  https://www.omnicalculator.com/physics/stress
+piece = SquarePrism(Elasticity.ALUMINUM.value,.1,5.0)
 #Solve_SimpleAxialTension(piece)
 #Solve_SimpleAxialTension(piece,displacement=.01)
-Solve_SimpleAxialTension(piece,force=400000)
+Solve_SimpleAxialTension(piece,force=50000)
+Solve_MultipleAxialTension(piece,[(10,50000)],10)
