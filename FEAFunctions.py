9283cc4515d85a8ee615a4b0832ed282b3506cfd
wh1ter0se <camelCaseEverything@gmail.com>
2021-12-22 02:26:39 -0600

    Started implementing axial torsion. Left some TODOs.

--- FEAFunctions.py
+++ FEAFunctions.py
@@ -1,6 +1,7 @@
 from numpy.lib.function_base import disp
 from Bodies import *
 import numpy as np
+import math
 
 # All equations are copied/derived from
 # https://enterfea.com/finite-element-analysis-by-hand/
@@ -18,7 +19,21 @@ def print_displacements(displacements,global_displacement=None):
         i += 1
     if global_displacement is not None:
         print("ΔL_global: " + str(round((10**3)*global_displacement,5)) + "mm")
-        
+
+def rad_to_deg(radians):
+    return (radians * (180/math.pi))
+
+def print_radial_displacements(displacements,global_displacement=None):
+    i = 0
+    for displacement in displacements:
+        print("Δθ_" + str(i+1) + ": " + str(round((10**3)*displacements[i],5)) + "degrees")
+        i += 1
+    if global_displacement is not None:
+        print("Δθ_global: " + str(round((10**3)*global_displacement,5)) + "degrees")
+
+# TODO add a function to convert displacements to strain (will require workpiece length to calculate)
+# TODO add a function to convert strain to stress (will require Young's Modulus to calculate)
+    
 # This is from the first simple example
 # Solves setup given EITHER force (N) or displacement (m)
 # Force and displacement are both numpy column vectors
@@ -38,12 +53,15 @@ def Solve_SimpleAxialTension(workpiece,force=None,displacement=None):
         total_displacement = abs(displacement_col[0])+abs(displacement_col[1])
         displacement_col = np.array([0,total_displacement]).T # left side (ΔL_1) is pinned
 
+    # TODO add another if chunk to run the calculations based on stress
+
     if force == None and displacement == None:
         print("No force/displacement given")
         return
 
     print_forces(force_col)
     print_displacements(displacement_col)
+    # TODO add a function to print stress/strain
 
 # Allows you to solve for local/global displacement, given pairs of locations and forces.
 # The area is assumed to be the same along the entire piece.
@@ -82,6 +100,7 @@ def Solve_MultipleAxialTension(workpiece,pos_force_pairs,nodes):
             #displacements[i] = 0
     print_forces(forces.T[0])
     print_displacements(displacements.T[0],global_displacement=sum(displacements.T[0]))
+    # TODO add a function to print stress/strain
     
 # Solves for cantilever deflection (fixed at one end, free at the other)
 # Displacement is perpendicular rather than parallel (as in tension)
@@ -112,6 +131,38 @@ def Solve_SimpleCantileverDeflection(workpiece,force=None,displacement=None):
 
     print_forces(force_col)
     print_displacements(displacement_col)
+    # TODO add a function to print stress/strain
+
+def Solve_SimpleAxialTorsion(workpiece,torque=None,rad_displacement=None):
+    #coeff = (3 * workpiece.modulus * workpiece.I_xx) / (workpiece.length**3) # P = (3EI / L^3)(delta)
+    # TODO calculate the correct coefficient for axial displacement due to torque
+    #      this might require adding another enum in Bodies for shear modulus
+    #      it would also probably require calculating J (second polar moment of inertia)
+    stiffness = (np.eye(2) * 2) - 1.0
+    stiffness *= coeff
+
+    if rad_displacement != None and torque == None: # displacement is given, solve for torque
+        rad_displacement_col = np.array([0,rad_displacement]).T # left side (Δθ_1) is pinned
+        force_col = np.matmul(stiffness,rad_displacement_col) # B = Ax
+
+    if torque != None and rad_displacement == None: # torque is given, solve for displacement
+        force_col = np.array([-torque,torque]).T # equal and opposite reaction torque
+        # stiffness is a singular matrix, so the method of least squares is needed to solve
+        rad_displacement_col = np.linalg.lstsq(stiffness,force_col,rcond=None)[0] 
+        total_displacement = abs(rad_displacement_col[0])+abs(rad_displacement_col[1])
+        rad_displacement_col = np.array([0,total_displacement]).T # left side (Δθ_1) is pinned
+
+    if torque == None and rad_displacement == None:
+        print("No force/displacement given")
+        return
+
+    if torque != None and rad_displacement != None:
+        print("error in this function")
+        return
+
+    rad_displacement_col = rad_to_deg(rad_displacement_col)
+    print_forces(force_col)
+    print_radial_displacements(rad_displacement_col)
 
 ## Function tests
 #  Results can be verified with
