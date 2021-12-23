136d5737bb2e5b693a76f8ce7ada9f2fea027345
wh1ter0se <camelCaseEverything@gmail.com>
2021-12-23 04:35:53 -0600

    Removed rinoh from the conf.pu (possibly temp).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['rinoh.frontend.sphinx',
-              'sphinx.ext.napoleon',
+extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.coverage']
